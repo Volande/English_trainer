642bb0293a2b59d4a86aec6e93d3214297e77cda
Volande <106276019+Volande@users.noreply.github.com>
2026-04-13 15:12:57 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,9 +2,29 @@ const CACHE_NAME = "vocab-app-v1";
 const urlsToCache = ["./","./index.html"];
 
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
 });
 
+// activate → чистимо старий кеш
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(k => {
+          if(k !== CACHE_NAME) return caches.delete(k);
+        })
+      )
+    )
+  );
+});
+
+// fetch → стратегія cache-first
 self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
